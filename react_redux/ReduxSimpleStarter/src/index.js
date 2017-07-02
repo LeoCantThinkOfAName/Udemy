77b7377629ee813920299dfaec862917e32f250f
leocantthinkofaname <i7851@hotmail.com>
2017-07-03 03:27:11 +0800

    clear git

---
+++ react_redux/ReduxSimpleStarter/src/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
+
+import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyC9Q9EVkG0MNcabJCbX5fkUF6qWsobC9v8';
+
+//Create a new component. This component should produce som HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('鄭嘉瑜');
+    };
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={ videoSearch } />
+                <VideoDetail video={ this.state.selectedVideo } />
+                <VideoList
+                    videos={ this.state.videos }
+                    onVideoSelect={ selectedVideo => this.setState({ selectedVideo }) }
+                />
+            </div>
+        );
+    };
+}
+
+//Take this component's generated HTML and put it on the page(in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
