22380b5f10738ff251370288cb092b3c099d6f26
leocantthinkofaname <i7851@hotmail.com>
2017-07-07 17:14:04 +0800

    progress update

---
+++ react_redux/ReduxSimpleStarter_booklist/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'item1', pages: 100},
+        {title: 'item2', pages: 200},
+        {title: 'item3', pages: 300},
+        {title: 'item4', pages: 400},
+        {title: 'item5', pages: 500}
+    ]
+}
